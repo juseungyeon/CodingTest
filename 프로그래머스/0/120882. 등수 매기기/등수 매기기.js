2bc96fc49b2b00e06c08e00b199edbae4cbb1f9a
주승연 <50696162+juseungyeon@users.noreply.github.com>
2024-09-11 14:29:11 +0900

    [level 0] Title: 등수 매기기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,6 @@
+function solution(score) {
+    const averages = score.map(e => (e[0] + e[1]) / 2);
+    const sorted = [...averages].sort((a, b) => b - a);
+    
+    return averages.map(avg => sorted.indexOf(avg) + 1);;
+}
