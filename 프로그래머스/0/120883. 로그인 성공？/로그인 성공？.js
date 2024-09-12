82d2e8c768c974ebd19ac1559ac62d2edb883444
주승연 <50696162+juseungyeon@users.noreply.github.com>
2024-09-12 16:00:39 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,12 @@
+function solution(id_pw, db) {
+    for (let user of db) {
+        if (user[0] === id_pw[0]) { 
+            if (user[1] === id_pw[1]) { 
+                return "login";
+            } else {
+                return "wrong pw";
+            }
+        }
+    }
+    return "fail"; 
+}
