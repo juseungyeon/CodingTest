c2373b985bb7cf784db9cae324db2331791bf43a
주승연 <50696162+juseungyeon@users.noreply.github.com>
2024-09-20 17:55:00 +0900

    [level 0] Title: 문자열 밀기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,12 +1,11 @@
 function solution(A, B) {
     let answer = 0;
-    let newA = A;
     
     if(A === B) return 0;
     for(let i=0; i<A.length; i++){
-        newA = newA.at(A.length-1) + newA.slice(0,-1);
+        A = A.at(A.length-1) + A.slice(0,-1);
         answer +=1;
-        if(newA === B) return answer;
+        if(A === B) return answer;
     }
-    if(answer === A.length) return -1;
+    return -1;
 }
