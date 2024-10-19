8344a4ec79d37e06a5748d40263aaa0463fe9109
주승연 <50696162+juseungyeon@users.noreply.github.com>
2024-10-19 13:57:02 +0900

    [level 1] Title: 시저 암호, Time: 0.99 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,15 +1,4 @@
 function solution(s, n) {
-    return s
-        .split('')
-        .map(char => {
-            if (char === ' ') return ' '; 
-            const charCode = char.charCodeAt(0);
-
-            if (char >= 'a' && char <= 'z') {
-                return String.fromCharCode((charCode - 97 + n) % 26 + 97);
-            } else if (char >= 'A' && char <= 'Z') {
-                return String.fromCharCode((charCode - 65 + n) % 26 + 65); 
-            }
-        })
-        .join('')
+    const chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY"
+    return s.split('').map(e => e === " " ? " " : chars[chars.indexOf(e) + n]).join('');
 }
